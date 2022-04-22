3b33cb69b7b9f1263d1e50f8c2a2cbbc6ff59b2c
Rostrv <trovatorosario91@gmail.com>
2022-04-22 16:19:07 +0200

    esercizio e bonus

---
+++ app.js
@@ -0,0 +1,59 @@
+const app = new Vue({
+    el: '#app',
+    data: {
+        newTask: [],
+
+        tasks: [{
+                text: 'learn Javascript',
+                done: false
+            },
+
+            {
+                text: 'go crazy',
+                done: true
+
+            },
+
+            {
+                text: 'clean camera lenses',
+                done: false
+            },
+
+            {
+                text: 'walk the dog',
+                done: true
+            }
+
+        ],
+
+    },
+
+    methods: {
+        addTask() {
+            console.log('add new task');
+            console.log(this.tasks)
+            this.tasks.push({ text: this.newTask, done: false })
+
+        },
+
+        removeTask(index) {
+            this.tasks.splice(index, 1)
+
+        },
+
+        changeDone(index) {
+
+            if (this.tasks[index].done == true)
+                this.tasks[index].done = false
+
+            else {
+                this.tasks[index].done = true
+            }
+
+        }
+
+
+    }
+
+
+});
